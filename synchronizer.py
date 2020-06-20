c5a05c149469dcfee6e6c878f496ca811acb7743
Patrick Hämmerle <patrick.haemmerle@gmail.com>
2020-06-21 00:46:32 +0200

    Sync new files in both directions

---
+++ synchronizer.py
@@ -0,0 +1,59 @@
+import json
+import subprocess
+
+
+class Synchronizer:
+
+    def __init__(self, root1, root2, rcloneCommand='rclone'):
+        self._root1 = root1
+        self._root2 = root2
+        self._rcloneCommand = rcloneCommand
+
+    def isSynced(self):
+        synced = subprocess.call([
+            self._rcloneCommand,
+            'check', self._root1,
+            self._root2
+        ])
+        return synced == 0
+
+    def synchronize(self):
+        list1 = self._getFileList(self._root1)
+        list2 = self._getFileList(self._root2)
+        self._copyMisingFiles(list1, list2, self._root1, self._root2)
+        self._copyMisingFiles(list2, list1, self._root2, self._root1)
+
+    def _copyMisingFiles(self, list1, list2, fromRoot, toRoot):
+        for path in list1:
+            if list2.get(path) == None:
+                self._copyFile(path, fromRoot, toRoot)
+
+    def _copyFile(self, path, fromRoot, toRoot):
+        arguments = [
+            self._rcloneCommand,
+            'copy',
+            fromRoot + '/' + path,
+            toRoot
+        ]
+        p = subprocess.call(arguments)
+        if p != 0:
+            raise Exception(f'Could not copy file {path} from {fromRoot} to {toRoot}')
+
+    def _getFileList(self, root):
+        arguments = [
+            self._rcloneCommand,
+            'lsjson',
+            '--hash',
+            root
+        ]
+        p = subprocess.Popen(arguments + [], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        r = p.communicate()
+        if p.returncode != 0:
+            raise ('Could not list files on one of the remotes')
+
+        list = json.loads(r[0])
+        dict = {}
+        for object in list:
+            dict[object['Path']] = object
+
+        return dict
--- synchronizer_test.py
+++ synchronizer_test.py
@@ -1,6 +1,68 @@
+import os
+import shutil
+import subprocess
 from unittest import TestCase
+
+from synchronizer import Synchronizer
 
 
 class TestSynchronizer(TestCase):
-    def testTest(self):
-        pass
+
+    def __init__(self, *args):
+        super(TestSynchronizer, self).__init__(*args)
+        self._rcloneCommand = 'rclone'
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        shutil.rmtree('test', ignore_errors=True, onerror=None)
+        os.makedirs('test/root1')
+        os.makedirs('test/root2')
+
+    @staticmethod
+    def getRoot1():
+        return 'test/root1'
+
+    @staticmethod
+    def getRoot2():
+        return 'test/root2'
+
+    @staticmethod
+    def createTextFile(root, path, content):
+        with open(root + '/' + path, 'w') as f:
+            f.write(content)
+
+    def getSynchronizer(self):
+        return Synchronizer(self.getRoot1(), self.getRoot2(), self._rcloneCommand)
+
+    def check(self, fixture):
+        synchronizer = self.getSynchronizer()
+        self.assertTrue(synchronizer.isSynced())
+        self.assertTrue(Synchronizer(fixture + '/expected/', self.getRoot1()).isSynced())
+        self.assertTrue(Synchronizer(fixture + '/expected/', self.getRoot2()).isSynced())
+
+    def prepareRoots(self, fixture):
+        # Assert that the return code is 0, to make sure that there are no errors while preparing the test cases
+        self.assertEqual(subprocess.call([self._rcloneCommand, 'sync', fixture + '/root1/', self.getRoot1()]), 0)
+        self.assertEqual(subprocess.call([self._rcloneCommand, 'sync', fixture + '/root2/', self.getRoot2()]), 0)
+
+    def testIsSyncedTrue(self):
+        self.prepareRoots('fixtures/isSynced/synced')
+        self.assertTrue(self.getSynchronizer().isSynced())
+
+    def testIsSyncedFalse(self):
+        self.prepareRoots('fixtures/isSynced/notSynced')
+        self.assertFalse(self.getSynchronizer().isSynced())
+
+    def testNewFileInRoot1(self):
+        self.prepareRoots('fixtures/newFileInRoot1')
+        self.getSynchronizer().synchronize()
+        self.createTextFile(self.getRoot1(), 'newFile.txt', 'newFile')
+        self.getSynchronizer().synchronize()
+        self.check('fixtures/newFileInRoot1')
+
+    def testNewFileInRoot2(self):
+        self.prepareRoots('fixtures/newFileInRoot2')
+        self.getSynchronizer().synchronize()
+        self.createTextFile(self.getRoot2(), 'newFile.txt', 'newFile')
+        self.getSynchronizer().synchronize()
+        self.check('fixtures/newFileInRoot2')
